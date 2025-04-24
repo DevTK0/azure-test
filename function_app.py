dc2549f8f50609cc4df43415236fa04606f8864c
Terence <tko.developer.02@gmail.com>
2025-04-24 15:54:12 +0800

    feat: Configure function to be anonymous

--- function_app.py
+++ function_app.py
@@ -2,6 +2,6 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.route(route="test")
+@app.route(route="test", auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("hello world")
